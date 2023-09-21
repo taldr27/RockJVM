791868fc0480cf7ae675486465ea1ba14fc0b9a8
Diego <diegogarciaq98@gmail.com>
2023-09-20 22:08:44 -0500

    Add complete section 2

--- src/main/scala/lectures/part1Basics/CBNvsCBV.scala
+++ src/main/scala/lectures/part1Basics/CBNvsCBV.scala
@@ -1,12 +1,12 @@
 package lectures.part1Basics
 
 object CBNvsCBV extends App {
-  def calledByValue(x: Long): Unit = {
+  private def calledByValue(x: Long): Unit = {
     println("by value: " + x)
     println("by value: " + x)
   }
 
-  def calledByName(x: => Long): Unit = {
+  private def calledByName(x: => Long): Unit = {
     println("by value: " + x)
     println("by value: " + x)
   }
@@ -14,8 +14,8 @@ object CBNvsCBV extends App {
   calledByValue(System.nanoTime())
   calledByName(System.nanoTime())
 
-  def infinite(): Int = 1 + infinite()
-  def printFirst(x: Int, y: => Int) = print(x)
+  private def infinite(): Int = 1 + infinite()
+  private def printFirst(x: Int, y: => Int) = print(x)
 
 //  printFirst(infinite(), 34)
   printFirst(34, infinite())

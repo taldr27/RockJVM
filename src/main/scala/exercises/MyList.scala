34de36c636390f2b0ed78430df29c13d02d63fb5
Diego <diegogarciaq98@gmail.com>
2023-10-15 22:08:39 -0500

    Add update section

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
 
   /*
     head = first element of the list
@@ -9,28 +9,42 @@ abstract class MyList {
     add(int) => new list with this element added
     toString => a string representation of the list
    */
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
+  def printElements: String
+  override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
 
-  def tail: MyList = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
 
   def isEmpty: Boolean = true
 
-  def add(element: Int): MyList = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
 
-  def tail: MyList = t
+  def tail: MyList[A] = t
 
   def isEmpty: Boolean = false
 
-  def add(element: Int): MyList = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
+  def printElements: String =
+    if(t.isEmpty) "" + h
+    else h + " " + t.printElements
+}
+
+object ListTest extends App {
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 }
---
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -0,0 +1,52 @@
+package lectures.part2oop
+
+object Generics extends App {
+  class MyList[+A] {
+    // use the type A
+    def add[B >: A](element: B): MyList[B] = ???
+    /*
+      A = Cat
+      B = Animal
+     */
+  }
+
+  class MyMap[Key, Value]
+
+  val listOfIntegers = new MyList[Int]
+  val listOfStrings = new MyList[String]
+
+  // generic methods
+  object MyList {
+    def empty[A]: MyList[A] = ???
+  }
+  val emptyListOfIntegers = MyList.empty[Int]
+
+  // variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // yes List[Cat] extends List[Animal] = COVARIANCE
+  class CovariantList[+A]
+  val animal: Animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  // animalList.add(new Dog) ???
+
+  // 2. No = INVARIANCE
+  class InvariantList[A]
+  val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+  // 3. Hell, no! CONTRAVARIANCE
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+
+  // bounded types
+  class Cage[A <: Animal](animal: A)
+  val cage = new Cage(new Dog)
+
+//  class Car
+//  val newCage = new Cage(new Car)
+
+
+}
